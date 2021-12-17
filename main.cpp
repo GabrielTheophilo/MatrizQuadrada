48b4e6c0d4a7920a44defba9d438811253a90229
Gabriel <gabrieltsf10@gmail.com>
2021-12-16 22:25:59 -0300

    Initial commit on C++ exercise/Functions ready TODO: Documentation&&main

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "funcoes.h"
+using namespace std;
+
+
+
+int main(void){
+    MatrizQuadrada M1, M2, M3,TRA,I;
+    leMatriz(M1);
+    leMatriz(M2);
+    //soma(M1,M2,M3);
+    produto(M1,M2,M3);
+    //imprimeMatriz(M1);
+    //criaTransposta(M1, TRA);
+    //if(compara(M1, M2)){
+    //    cout << "Matrizes iguais";
+    //}
+    //else{
+    //    cout << "Matrizes diferentes";
+    //}
+    //criaMatrizIdentidade(I, 3);
+    //imprimeMatriz(I);
+    //produto();
+
+}
