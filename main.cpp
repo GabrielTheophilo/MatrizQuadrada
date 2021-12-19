ce8488e3265ed4b132b079dadb1f89ccee09bd2c
Gabriel <gabrieltsf10@gmail.com>
2021-12-19 03:26:13 -0300

    Improved documentation

--- main.cpp
+++ main.cpp
@@ -4,15 +4,27 @@
  *   
  *   Criado por: Gabriel Theophilo de Souza Figueira - gabriel.figueira@ufv.br
  *   16/12/2021
- *   
- *   
+ *     
+ *   Função; -> Comentário
+ *      @ input
+ * 
+ *   Funções Dispoíveis para chamada:
+ *   leMatriz( MatrizQuadrada ); -> Recebe um struct da matriz quadrada e lê as dimensões e as posições
+ *      @ (Número de dimensões) (Inteiro da posição 0x0) (Inteiro da posição 0x1) ... @
+ * 
+ *   criaMatrizIdentidade( MatrizQuadrada , Dimensão ); -> Recebe uma matriz(vazia) e o número de linhas/colunas e cria uma matriz identidade
+ *      @ Sem input na execução @
+ * 
+ *   compara( MatrizQuadrada , MatrizQuadrada ); -> Percorre os elementos das matrizes para comparar se são Matrizes idênticas
+ *      @ Sem input na execução @
+ * 
+ *   criaTransposta ( MatrizQuadrada, MatrizQuadrada ); ->
+ * 
+ *  
+ *   produto( MatrizQuadrada, MatrizQuadrada, MatrizQuadrada ); ->
  * 
  * 
- * 
- * 
- * 
- * 
- * 
+ *   soma( MatrizQuadrada, MatrizQuadrada, MatrizQuadrada ); ->
  * 
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 */
